63c29714f144fa9a5e5d1da9c2c278f8c9d568a5
novakw267 <novakw267@gmail.com>
2017-01-26 10:34:31 -0500

    adding a win check

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -5,11 +5,29 @@ const board = [0, 1, 2,
                6, 7, 8,
              ];
 
-// const winConditions = [
-// [0, 1, 2], [3, 4, 5], [6, 7, 8], //rows
-// [0, 3, 6], [1, 4, 7], [2, 5, 8], //columns
-// [0, 4, 8], [2, 4, 6] //diagnol
-// ];
+const winConditions = [
+[0, 1, 2], [3, 4, 5], [6, 7, 8], //rows
+[0, 3, 6], [1, 4, 7], [2, 5, 8], //columns
+[0, 4, 8], [2, 4, 6] //diagnol
+];
+
+const players = [
+   '1',
+   '2',
+ ];
+
+const threeInARow = function (player, cellOne, cellTwo, cellThree) {
+    return (cellOne === player) && (cellTwo === player) && (cellThree === player);
+};
+}
+
+const winRows = function (player) {
+  return threeInARow
+}
+
+
+
+
 
 const printBoard = function(board) {
   for (let i = 0; i < board.length; i+=3) {
@@ -27,10 +45,7 @@ const turn = function (player, move) {
 
 // const newGame = [];
 //
-// const players = [
-//   'x',
-//   'o',
-// ];
+
 //
 // const turn = {
 //   player: '1, 2',
@@ -64,9 +79,15 @@ turn(1, 4);
 printBoard(board);
 turn(2, 7);
 printBoard(board);
-turn(1, 0);
+turn(1, 5);
 printBoard(board);
+turn(2, 8);
+printBoard(board);
+turn(1, 3);
+printBoard(board);
+checkWin('x');
 
 module.exports = {
   printBoard,
+  checkWin,
 };
