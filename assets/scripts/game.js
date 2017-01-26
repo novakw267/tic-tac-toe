ddd4c9c1490345a2f427cec55be74f2821cfc981
novakw267 <novakw267@gmail.com>
2017-01-26 15:13:42 -0500

    fixed a lot of nag style errors still working on the win function

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -5,31 +5,42 @@ const board = [0, 1, 2,
                6, 7, 8,
              ];
 
+// const board = ['', '', '',
+//                '', '', '',
+//                '', '', '',
+//              ];
+
 const winConditions = [
-[0, 1, 2], [3, 4, 5], [6, 7, 8], //rows
-[0, 3, 6], [1, 4, 7], [2, 5, 8], //columns
-[0, 4, 8], [2, 4, 6] //diagnol
+[0, 1, 2], [3, 4, 5,], [6, 7, 8], //rows
+[0, 3, 6], [1, 4, 7,], [2, 5, 8], //columns
+[0, 4, 8], [2, 4, 6,] //diagnol
 ];
 
-const player1 = {
-  symbol: 'X'
+// const player1 = 'X';
+//
+// const player2 = 'O';
+
+
+const player = {
+    symbol: 'X',
 };
 
-const player2 = {
-  symbol: 'O'
-};
 
-const togglePlayer = function () {
-   if (this.player === 'X') {
-     this.player = 'O';
-   }
-   else {
-     this.player = 'X';
-   }
- };
+// const togglePlayer = function () {
+//    if (this.player === 'X')
+//      this.player = 'O';
+//    }
+//    else {
+//      this.player = 'X';
+//    }
+//  };
+
+
 
 const threeInARow = function (player, cellOne, cellTwo, cellThree) {
-    return (cellOne === player) && (cellTwo === player) && (cellThree === player);
+    if ((cellOne === player) && (cellTwo === player) && (cellThree === player)) {
+        console.log(player + ' ' + "Wins!");
+    }
 };
 
 const winRow = function (player) {
@@ -39,40 +50,42 @@ const winRow = function (player) {
 };
 
 const winColum = function (player) {
-  return  threeInARow(player, board[0]), (board[3]), (board[6]) ||
-          threeInARow(player, board[1]), (board[4]), (board[7]) ||
-          threeInARow(player, board[2]), (board[5]), (board[8]);
+  return threeInARow(player, board[0]), (board[3]), (board[6]) ||
+         threeInARow(player, board[1]), (board[4]), (board[7]) ||
+         threeInARow(player, board[2]), (board[5]), (board[8]);
 };
 
 const winDiag = function (player) {
-  return   threeInARow(player, board[0]), (board[4]), (board[8]) ||
-           threeInARow(player, board[2]), (board[4]), (board[6]);
+  return threeInARow(player, board[0]), (board[4]), (board[8]) ||
+         threeInARow(player, board[2]), (board[4]), (board[6]);
 };
 
 const winnerIs = function (player) {
-  return winRow(player.symbol) || winColum(player.symbol) || winDiag(player.symbol);
+  if (winRow(player) || winColum(player) || winDiag(player)) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 const getWinner = function () {
-  if (winnerIs (player1)) {
+  if (winnerIs(player) === true) {
     return ('Winner is X');
-  }
-  else if (winnerIs(player2)) {
+  } else if (winnerIs(player) === true) {
     return ('Winner is O');
-  }
-  else { return -1;}
+  } else { return null;}
 };
 
 
 
-const printBoard = function(board) {
-  for (let i = 0; i < board.length; i+=3) {
-    console.log(board[i] + " " + board[i+1] + " " + board[i+ 2]);
+const printBoard = function (board) {
+  for (let i = 0; i < board.length; i += 3) {
+    console.log(board[i] + ' ' + board[i + 1] + ' ' + board[i + 2]);
   }
 };
 
 const turn = function (player, move, board) {
-  board[move]=player.symbol;
+  board[move] = player.symbol;
   if (getWinner(board) !== -1) {
     console.log(getWinner());
   }
@@ -86,8 +99,7 @@ const turn = function (player, move, board) {
 // const toggleTurn = function () {
 //   if (this.player === '1'){
 //     return this.player === '2';
-//   }
-//   else if (this.player === '2') {
+//   } else if (this.player === '2') {
 //     return this.player === '1';
 //   }
 //
@@ -106,20 +118,19 @@ const turn = function (player, move, board) {
 // };
 
 printBoard(board);
-turn(player1, 4, board);
+turn(player, 4, board);
 printBoard(board);
-turn(player2, 7, board);
+turn(player, 7, board);
 printBoard(board);
-turn(player1, 5, board);
+turn(player, 5, board);
 printBoard(board);
-turn(player2, 8, board);
+turn(player, 8, board);
 printBoard(board);
-turn(player1, 3, board);
+turn(player, 3, board);
 printBoard(board);
 module.exports = {
   printBoard,
   winnerIs,
-  togglePlayer,
   winConditions,
- getWinner,
+  getWinner,
 };
