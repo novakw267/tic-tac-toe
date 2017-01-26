89fe9c643d9ac04849ba8e24f8f6c5933da66cb5
novakw267 <novakw267@gmail.com>
2017-01-26 09:38:05 -0500

    added a print function next is working on win scenarios

---
+++ assets/scripts/game.js
@@ -0,0 +1,72 @@
+'use strict';
+
+const board = [0, 1, 2,
+               3, 4, 5,
+               6, 7, 8,
+             ];
+
+// const winConditions = [
+// [0, 1, 2], [3, 4, 5], [6, 7, 8], //rows
+// [0, 3, 6], [1, 4, 7], [2, 5, 8], //columns
+// [0, 4, 8], [2, 4, 6] //diagnol
+// ];
+
+const printBoard = function(board) {
+  for (let i = 0; i < board.length; i+=3) {
+    console.log(board[i] + " " + board[i+1] + " " + board[i+ 2]);
+  }
+};
+
+const turn = function (player, move) {
+  if (player === 1) {
+    board[move] = 'X';
+  } else {
+    board[move] = 'O';
+  }
+};
+
+// const newGame = [];
+//
+// const players = [
+//   'x',
+//   'o',
+// ];
+//
+// const turn = {
+//   player: '1, 2',
+//   token: 'x, o',
+// };
+//
+// const toggleTurn = function () {
+//   if (this.player === '1'){
+//     return this.player === '2';
+//   }
+//   else if (this.player === '2') {
+//     return this.player === '1';
+//   }
+//
+//   return this.player;
+// }
+//
+//
+//
+//
+// const reset = function () {
+//   if (board === []) {
+//     return 'empty';
+//   } else {
+//     return newGame;
+//   }
+// };
+
+printBoard(board);
+turn(1, 4);
+printBoard(board);
+turn(2, 7);
+printBoard(board);
+turn(1, 0);
+printBoard(board);
+
+module.exports = {
+  printBoard,
+};
