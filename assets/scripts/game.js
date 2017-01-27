4845eaab4990c7fd87a8cc25e1b611f7911c97ec
novakw267 <novakw267@gmail.com>
2017-01-27 15:19:30 -0500

    making another commit after style changes

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -13,7 +13,7 @@ let newBoard = ['', '', '',
 let fullBoard = false;
 
 const spotTaken = function (element) {
-   return element !== '';
+    return element !== '';
 };
 
 const boardFull = function () {
@@ -25,14 +25,15 @@ const boardFull = function () {
 const player1 = 'X';
 const player2 = 'O';
 let currentPlayer = 'X';
+
 //let turn = currentPlayer.board;
 
 const togglePlayer = function () {
   if (currentPlayer === 'X') {
     currentPlayer = 'O';
-  } else if (currentPlayer === 'O'){
-   currentPlayer = 'X';
- }
+  } else if (currentPlayer === 'O') {
+    currentPlayer = 'X';
+  }
 };
 
 const reset = function () {
@@ -45,27 +46,30 @@ const threeInARow = function (player, cellOne, cellTwo, cellThree) {
 };
 
 const winRows = function (currentPlayer) {
-  if (threeInARow(currentPlayer, board[0], board[1], board[2])||
-         threeInARow(currentPlayer, board[3], board[4], board[5])||
-         threeInARow(currentPlayer, board[6], board[7], board[8])) {
+  if (threeInARow(currentPlayer, board[0], board[1], board[2]) ||
+      threeInARow(currentPlayer, board[3], board[4], board[5]) ||
+      threeInARow(currentPlayer, board[6], board[7], board[8])) {
 
          console.log('Great job ' + currentPlayer + ' you win!');
     }
+
 };
 
 const winColumn = function (currentPlayer) {
   if (threeInARow(currentPlayer, board[0], board[3], board[6]) ||
-         threeInARow(currentPlayer, board[1], board[4], board[7]) ||
-         threeInARow(currentPlayer, board[2], board[5], board[8])) {
+      threeInARow(currentPlayer, board[1], board[4], board[7]) ||
+      threeInARow(currentPlayer, board[2], board[5], board[8])) {
          console.log('Great job ' + currentPlayer + ' you win!');
     }
+
 };
 
 const winDiag = function (currentPlayer) {
   if (threeInARow(currentPlayer, board[0], board[4], board[8]) ||
-         threeInARow(currentPlayer, board[2], board[4], board[6])) {
-           console.log('Great job ' + currentPlayer + ' you win!');
+      threeInARow(currentPlayer, board[2], board[4], board[6])) {
+         console.log('Great job ' + currentPlayer + ' you win!');
          }
+
 };
 
 const winnerIs = function (currentPlayer) {
@@ -73,22 +77,24 @@ const winnerIs = function (currentPlayer) {
 };
 
 let tieGame = function (boardFull, currentPlayer) {
-  if (fullBoard === true && winRows(currentPlayer) === false && winColumn(currentPlayer) === false && winDiag(currentPlayer) === false) {
-  tieGame = true;
+    if (fullBoard === true && winRows(currentPlayer) === false && winColumn(currentPlayer) === false && winDiag(currentPlayer) === false) {
+        tieGame = true;
    }
 };
 
 const getWinner = function () {
   if (tieGame(fullBoard, currentPlayer)) {
     console.log('Game is a tie, try again.');
-  }  else if (winnerIs(player1)) {
-    return currentPlayer;
-  } else if (winnerIs(player2)) {
+  }
+    else if (winnerIs(player1)) {
     return currentPlayer;
   }
+    else if (winnerIs(player2)) {
+    return currentPlayer;
+  }
+
   reset();
 };
-
 
 const yourMove = function (board, move) {
       if (board[move] !== '') {
